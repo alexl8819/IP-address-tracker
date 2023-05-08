2dcd4c3102e82f900308bceb1618a4803d9ed4c5
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2023-05-07 20:37:36 -0700

    fixes map not updating when positions are changed

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 import L from 'leaflet';
 import { useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
 import PropTypes from 'prop-types';
 
 import icon from 'leaflet/dist/images/marker-icon.png';
@@ -15,22 +15,12 @@ L.Marker.prototype.options.icon = L.icon({
 });
 
 function LocationMarker ({ coords }) {
-  const [position, setPosition] = useState(!coords.length ? null : coords)
-  const map = useMapEvents({
-    click() {
-      map.locate()
-    },
-    locationfound(e) {
-      setPosition(e.latlng)
-      map.flyTo(e.latlng, map.getZoom())
-    },
-  });
-
-  return position === null ? null : (
-    <Marker position={position}>
-      <Popup>You are here</Popup>
-    </Marker>
-  )
+  const map = useMap();
+  map.flyTo(coords, map.getZoom());
+  
+  return !coords.length === null ? null : (
+    <Marker position={coords}></Marker>
+  );
 }
 
 export default function Map ({ coords }) {
@@ -44,7 +34,6 @@ export default function Map ({ coords }) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-
       <LocationMarker coords={coords}></LocationMarker>
     </MapContainer>
   );
