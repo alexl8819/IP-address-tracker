f184f2d2e765df1d23f1633cc50fa75ee4c4878f
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2023-05-22 13:23:31 -0700

    enable retina detection for tile layers

--- src/components/Map.js
+++ src/components/Map.js
@@ -17,6 +17,7 @@ export default function TrackerMap ({ coords }) {
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        detectRetina={true}
       />
       <LocationMarker coords={usedCoords} marker={customMarker}></LocationMarker>
     </MapContainer>
